db162de4330d74b90f1a0293f7d10357fcc4a1c5
FrancoAyala <95264989+FrancoAyala@users.noreply.github.com>
2025-11-24 22:24:00 -0300

    feat: add hive ddl for ejercicio 2

---
+++ ejercicio-2/hive/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS car_rental_db;
+USE car_rental_db;
+
+DROP TABLE IF EXISTS car_rental_analytics;
+
+CREATE EXTERNAL TABLE car_rental_analytics (
+  fuelType           STRING,
+  rating             INT,
+  renterTripsTaken   INT,
+  reviewCount        INT,
+  city               STRING,
+  state_name         STRING,
+  owner_id           INT,
+  rate_daily         INT,
+  make               STRING,
+  model              STRING,
+  year               INT
+)
+STORED AS PARQUET
+LOCATION 'hdfs:///car_rental/curated/analytics/';
